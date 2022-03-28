2cebcb8c6512bb257b9ada161005fd8359d3ec35
Aaron Hardy <aaron@aaronhardy.com>
2022-03-27 22:20:55 -0600

    Begin new design.

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	"use strict";
+    
+    // 5. welcome animation support
+
+        $(window).load(function(){
+        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+        $(window).load(function(){
+        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+        });
+
+});	
